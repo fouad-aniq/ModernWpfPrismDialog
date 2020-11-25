53affee401140b11e2265687b5d17017ce979d5a
psisco2 <46678564+psisco2@users.noreply.github.com>
2020-11-25 14:42:46 +0100

    Add files via upload

---
+++ ModuleA/ViewModels/DialogCViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ModernWpfPrismDialog.Core;
+using Prism.Services.Dialogs;
+
+namespace ModuleA.ViewModels
+{
+    public class DialogCViewModel : MyDialogAware
+    {
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { SetProperty(ref message, value); }
+        }
+        public DialogCViewModel()
+        {
+            Title = "Welcom to Dialog C";
+            Message = "Hello world. this is dialog c. opened with prism mvvm....";
+        }
+    }
+}
---
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+
+namespace ModuleA.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        private IDialogService dialogService;
+
+        public DelegateCommand OpenDialogBCommand { get; private set; }
+
+        public ViewAViewModel(IDialogService _dialogService)
+        {
+            Message = "View A from your Prism Module";
+
+            dialogService = _dialogService;
+            OpenDialogBCommand = new DelegateCommand(OpenDialogB);
+        }
+
+        private void OpenDialogB()
+        {
+            var param = new DialogParameters();
+            param.Add("message", "Fouad");
+
+            dialogService.Show("DialogC", param, r =>
+              {
+                  if (r.Result == ButtonResult.OK)
+                      Message = "Message OK";
+                  else
+                      Message = "Message Non OK";
+              });
+        }
+    }
+}
