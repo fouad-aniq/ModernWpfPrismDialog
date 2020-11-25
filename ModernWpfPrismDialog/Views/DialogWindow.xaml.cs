e1abc28342d789cf68871c29bbb0fb0a3b6d695a
psisco2 <46678564+psisco2@users.noreply.github.com>
2020-11-25 14:45:45 +0100

    Add files via upload

---
+++ ModernWpfPrismDialog/Views/DialogWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using ModernWpf;
+using ModernWpf.Controls;
+using Prism.Services.Dialogs;
+using System;
+using System.ComponentModel;
+
+namespace ModernWpfPrismDialog.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : ContentDialog, IDialogWindow
+    {
+        public DialogWindow()
+        {
+            InitializeComponent();
+        }
+
+        public IDialogResult Result { get; set; }
+
+        event EventHandler IDialogWindow.Closed
+        {
+            add
+            {
+                
+            }
+
+            remove
+            {
+
+            }
+        }
+
+        event CancelEventHandler IDialogWindow.Closing
+        {
+            add
+            {
+
+            }
+
+            remove
+            {
+
+            }
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void Show()
+        {
+            ShowAsync();
+        }
+
+        public bool? ShowDialog()
+        {
+            ShowAsync();
+
+            return false;
+        }
+    }
+}
